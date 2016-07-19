a855640a2378f98719dd68a6c11c8db01ab65e51
kshakirov <kshakirov@zoral.com.ua>
2016-07-19 16:03:14 +0300

    WIP #1 prices added

--- lib/src/attr_readers/composite_attrs_reader_1.rb
+++ lib/src/attr_readers/composite_attrs_reader_1.rb
@@ -1,6 +1,24 @@
 class CompositAttrsReader
 
 
+  def adjuct_bom_group_price boms, group_id
+    unless boms.nil?
+      boms.each_with_index do |value, index|
+        unless value[:prices].nil?
+          boms[index][:prices] = value[:prices][group_id]
+        end
+      end
+    end
+  end
+
+  def remove_bom_price boms
+    unless boms.nil?
+      boms.each_with_index do |value,index|
+        boms[index][:prices] = 'login'
+      end
+    end
+  end
+
   def add_ti_part_number ti_sku
     ti_part_numbers = []
     if ti_sku.size > 0
@@ -12,6 +30,17 @@ class CompositAttrsReader
       end
     end
     ti_part_numbers
+  end
+
+
+  def add_prices_to_bom_response boms, prices
+    boms.each_with_index  do |bom, index|
+      prices.each do |price|
+        if price['partId'] == bom[:sku]
+          boms[index][:prices] = price['prices']
+        end
+      end
+    end
   end
 
 
@@ -28,7 +57,7 @@ class CompositAttrsReader
         boms[index][:name] = part.name || boms[index][:part_type] + '-' + boms[index][:part_number]
 
       end
-      #add_prices_to_response(boms, get_prices(ids))
+      add_prices_to_bom_response(boms, get_prices(ids))
       boms
     else
       nil
@@ -37,9 +66,28 @@ class CompositAttrsReader
   end
 
 
-  def get_bom_attribute sku, id=''
+  def _get_bom_without_prices sku
+    boms = @bom_reader.get_attribute sku
+    add_standard_attrs_2_bom boms
+  end
+
+
+  def _get_bom_with_prices sku, id
     group_id = @decriptor.get_customer_group id
     boms = @bom_reader.get_attribute sku
     add_standard_attrs_2_bom boms
+    if group_id=='no stats'
+      remove_bom_price boms
+    else
+      adjuct_bom_group_price boms, @group_prices_map[group_id]
+    end
+  end
+
+  def get_bom_attribute sku, id
+    if id.nil?
+      _get_bom_without_prices sku
+    else
+      _get_bom_with_prices sku, id
+    end
   end
 end
--- test/composite_attr_reader.rb
+++ test/composite_attr_reader.rb
@@ -1,19 +1,19 @@
 require_relative "test_helper"
 
 class TestCompositeAttrsReader < MiniTest::Unit::TestCase
-  # def test_where_used
-  #   @stdreader = CompositAttrsReader.new
-  #   attrs = @stdreader.get_where_used_attribute 49639, 'sVrXIqos994v0pkehHI28Q=='
-  #   assert_equal  'Cartridge', attrs[4742][:partType]
-  #   assert_equal  'CHRA, CT10', attrs[6673][:description]
-  #   assert_equal  117.8, attrs[6673][:prices]
-  #
-  # end
-  # def test_bom
-  #   @stdreader = CompositAttrsReader.new
-  #   attrs = @stdreader.get_bom_attribute 6392, 'sVrXIqos994v0pkehHI28Q=='
-  #   p attrs
-  # end
+  def test_where_used
+    @stdreader = CompositAttrsReader.new
+    attrs = @stdreader.get_where_used_attribute 49639, 'sVrXIqos994v0pkehHI28Q=='
+    assert_equal  'Cartridge', attrs[4742][:partType]
+    assert_equal  'CHRA, CT10', attrs[6673][:description]
+    assert_equal  117.8, attrs[6673][:prices]
+
+  end
+  def test_bom
+    @stdreader = CompositAttrsReader.new
+    attrs = @stdreader.get_bom_attribute 6392, 'sVrXIqos994v0pkehHI28Q=='
+    p attrs
+  end
 
 
   def test_interchanges
@@ -24,12 +24,12 @@ class TestCompositeAttrsReader < MiniTest::Unit::TestCase
     assert attrs[0].key? :description
   end
 
-  # def test_kit_matrix
-  #   @stdreader = CompositAttrsReader.new
-  #   attrs = @stdreader.get_kit_matrix 63194
-  #   p attrs
-  #   assert_equal 2, attrs.size
-  #   assert attrs[0].key? :description
-  # end
+  def test_kit_matrix
+    @stdreader = CompositAttrsReader.new
+    attrs = @stdreader.get_kit_matrix 63194
+    p attrs
+    assert_equal 2, attrs.size
+    assert attrs[0].key? :description
+  end
 
 end

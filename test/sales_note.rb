aad388644a8acca121674e00d560cb2cad1c5f6a
kshakirov <kshakirov@zoral.com.ua>
2016-09-06 16:05:41 +0300

    WIP #3 tests fixed

--- test/sales_note.rb
+++ test/sales_note.rb
@@ -1,12 +1,17 @@
 require_relative "test_helper"
 
 class TestSalesNotesAttrsReader < MiniTest::Unit::TestCase
-  def test_bom
-    @reader = SalesNoteAttrReader.new
-    attrs = @reader.get_attribute 16572
+  def test_sales_note_getter
+    getter = SalesNoteGetter.new
+    attrs = getter.get_sales_note_attribute 16572
     assert_equal 2, attrs.size
-    #assert_equal 1983, attrs[1][:id]
+    assert_equal 348, attrs[1][:id]
+    assert_equal 'Left side of Audi twin turbo.' , attrs[1][:comment]
+  end
 
+  def test_sales_note_setter
+    setter = SalesNoteSetter.new
+    setter.set_sales_note_attribute 16572
   end
 
 end

03bbc56e1721fd941289924b62b57bb597c7ccc8
kshakirov <kshakirov@zoral.com.ua>
2016-09-13 18:27:15 +0300

    WIP #4 tests

--- test/service_kits.rb
+++ test/service_kits.rb
@@ -3,15 +3,15 @@ require_relative "test_helper"
 class TestServiceKitsAttrsReader < MiniTest::Unit::TestCase
   def test_service_kit_setter
     setter = ServiceKitSetter.new
-    #setter.set_service_kit_attribute 13958
+    setter.set_service_kit_attribute 13958
     setter.set_service_kit_attribute 6703
   end
 
-  # def test_service_kit_gettter
-  #   getter = ServiceKitGetter.new
-  #   attrs = getter.get_service_kit_attribute  13958,  'sVrXIqos994v0pkehHI28Q=='
-  #   assert_equal 49, attrs.size
-  #   assert_equal 42.68,   attrs.last[:prices]
-  # end
+  def test_service_kit_gettter
+    getter = ServiceKitGetter.new
+    attrs = getter.get_service_kit_attribute  13958,  'sVrXIqos994v0pkehHI28Q=='
+    assert_equal 49, attrs.size
+    assert_equal 42.68,   attrs.last[:prices]
+  end
 
 end

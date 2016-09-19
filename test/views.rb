30bb25ed29dce43598eb5c170f0b7a2c02768ef6
kshakirov <kshakirov@zoral.com.ua>
2016-09-19 16:30:42 +0300

    TEST 2 db

--- test/views.rb
+++ test/views.rb
@@ -1,17 +1,13 @@
 require 'minitest/autorun'
 require_relative "test_helper"
-require_relative "../lib/sync"
 
 class TestM_Views < Minitest::Unit::TestCase
-  def test_where_used
 
-
-  end
 
   def test_part
     part = Part.find 6392
     refute_nil part, "Part is nil"
-    assert_equal "test", part.name
+    assert_equal 6392, part.id
   end
 
 end

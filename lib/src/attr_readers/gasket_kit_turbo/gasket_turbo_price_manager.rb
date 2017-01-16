4101eefec27d144afda16ed2af0b5aa3e5dfcfaa
kshakirov <kshakirov@zoral.com.ua>
2017-01-16 19:29:29 +0200

    WIP #15 gasket kit turbo price fixed

--- lib/src/attr_readers/gasket_kit_turbo/gasket_turbo_price_manager.rb
+++ lib/src/attr_readers/gasket_kit_turbo/gasket_turbo_price_manager.rb
@@ -8,7 +8,7 @@ class GasketTurboPriceManager
     group_id = @group_price.prices[id]
     if turbo
         if  turbo[:prices]
-          turbo[:prices] = turbo[:prices][group_id.to_sym]
+          turbo[:prices] = turbo[:prices][:prices][group_id.to_sym]
         end
     end
     turbo

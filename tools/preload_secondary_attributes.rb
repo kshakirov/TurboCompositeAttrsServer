91a9b3d0a791ab83b51156b3d67f7a30c244e383
kshakirov <kshakirov@zoral.com.ua>
2017-01-18 15:45:22 +0200

    DONE GENERAL turbo gasket script

--- tools/preload_secondary_attributes.rb
+++ tools/preload_secondary_attributes.rb
@@ -1,7 +1,7 @@
 require_relative 'tools_helper'
 
-@logger.info("Preload: Starting  preloading attributes ")
+@logger.info("Preload: Starting  preloading  secondary attributes ")
 @products_collection = ProductsCollection.new
 since = ARGV[0].to_i || 0
 @products_collection.cache_secondary_attrs since
-@logger.info("Preload: Ending preloading  attributes")
+@logger.info("Preload: Ending preloading  secondary attributes")

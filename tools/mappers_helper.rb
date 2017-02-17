2ee5bf64b142d8beab734cf0bec8c884b980eb71
kshakirov <kshakirov@zoral.com.ua>
2017-02-17 18:33:13 +0200

    DONE #20 done multithreading config separate temporally

--- tools/mappers_helper.rb
+++ tools/mappers_helper.rb
@@ -7,7 +7,7 @@ require 'redis'
 require 'composite_primary_keys'
 require 'logger'
 require_relative "../lib/server.rb"
-configuration = YAML::load(IO.read(__dir__ + '/database.yml'))
+configuration = YAML::load(IO.read(__dir__ + '/price/database.yml'))
 ActiveRecord::Base.establish_connection(configuration[ENV['RACK_ENV']])
 @logger = Logger.new(__dir__ + '../../logs/cron.log')
 @logger.level =Logger::INFO

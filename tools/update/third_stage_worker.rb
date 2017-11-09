b6adf6c9cc130eedbb1fa04904ee6e59cb3fec3a
kshakirov <kshakirov@zoral.com.ua>
2017-11-09 18:49:29 +0200

    #220 gasket kit turbo added

--- tools/update/third_stage_worker.rb
+++ tools/update/third_stage_worker.rb
@@ -3,10 +3,12 @@ class ThirdStageWorker
   def initialize redis_cache
     @kit_matrix_setter = KitMatrixSetter.new redis_cache
     @application_setter = ApplicationSetter.new redis_cache
+    @gasket_turbo_setter = GasketTurboSetter.new redis_cache
   end
   def set_attribute sku
     @kit_matrix_setter.set_kit_matrix_attribute  sku
     @application_setter.set_application_attribute sku
+    @gasket_turbo_setter.set_gasket_turbo_attribute sku
     ActiveRecord::Base.clear_active_connections!
     "Future Finished sku [#{sku}]"
   end

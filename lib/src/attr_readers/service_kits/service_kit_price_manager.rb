1c52bc92e043392a1067fcff66723d5d8906263b
kshakirov <kshakirov@zoral.com.ua>
2017-11-14 12:16:32 +0200

    #250 service kits false error

--- lib/src/attr_readers/service_kits/service_kit_price_manager.rb
+++ lib/src/attr_readers/service_kits/service_kit_price_manager.rb
@@ -3,7 +3,7 @@ class ServiceKitPriceManager
     @group_price = GroupPrices.new
   end
   def add_group_price sks, id
-    unless sks.nil?
+    if sks.class.name=="Array"
       sks.each_with_index do |value, index|
         unless value[:prices].nil?
           sks[index][:prices] = value[:prices][id.to_sym]

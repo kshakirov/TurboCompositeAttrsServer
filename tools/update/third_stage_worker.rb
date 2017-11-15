5ed0f5a3bb36ede1dfc14544d4df8ecbe190d5d1
kshakirov <kshakirov@zoral.com.ua>
2017-11-15 19:07:47 +0200

    WIP #241 fuel type fixed

--- tools/update/third_stage_worker.rb
+++ tools/update/third_stage_worker.rb
@@ -2,12 +2,10 @@ class ThirdStageWorker
   include Celluloid
   def initialize redis_cache
     @kit_matrix_setter = KitMatrixSetter.new redis_cache
-    @application_setter = ApplicationSetter.new redis_cache
     @gasket_turbo_setter = GasketTurboSetter.new redis_cache
   end
   def set_attribute sku
     @kit_matrix_setter.set_kit_matrix_attribute  sku
-    @application_setter.set_application_attribute sku
     @gasket_turbo_setter.set_gasket_turbo_attribute sku
     ActiveRecord::Base.clear_active_connections!
     "Future Finished sku [#{sku}]"
